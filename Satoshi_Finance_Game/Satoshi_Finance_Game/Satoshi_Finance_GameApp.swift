f5f77878ca6df4a692c722ebc2c358bcf28636d0
Sousatoshiapp <fabio@sousatoshi.com>
2025-07-05 12:45:35 -0300

    Add files via upload

---
+++ Satoshi_Finance_Game/Satoshi_Finance_Game/Satoshi_Finance_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Satoshi_Finance_GameApp.swift
+//  Satoshi_Finance_Game
+//
+//  Created by Fabio Asdurian on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Satoshi_Finance_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Satoshi_Finance_Game/Satoshi_Finance_GameUITests/Satoshi_Finance_GameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Satoshi_Finance_GameUITestsLaunchTests.swift
+//  Satoshi_Finance_GameUITests
+//
+//  Created by Fabio Asdurian on 05/07/25.
+//
+
+import XCTest
+
+final class Satoshi_Finance_GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
